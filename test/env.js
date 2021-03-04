8f2ce0459744719458201f42aa51bce02ad0c003
nlf <quitlahok@gmail.com>
2021-03-04 12:30:08 -0800

    fix: default to accepting new ssh host key fingerprints
    PR-URL: https://github.com/npm/git/pull/7
    Credit: @nlf
    Close: #7
    Reviewed-by: @isaacs

--- test/env.js
+++ test/env.js
@@ -7,4 +7,5 @@ process.env.GIT_ASKPASS = 'foo'
 const ge = gitEnv()
 t.equal(ge.GIT_ASKPASS, 'foo', 'askpass makes it through')
 t.equal(ge.GIT_UNKNOWN_THINGIE, undefined, 'unknown thing filtered out')
+t.equal(ge.GIT_SSH_COMMAND, 'ssh -oStrictHostKeyChecking=accept-new')
 t.equal(gitEnv(), ge, 'memoized')
