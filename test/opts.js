a1de53710c629d699a2cdbdf97fb656e2ce36b28
Gar <gar+gh@danger.computer>
2021-04-09 10:52:45 -0700

    fix(env): stop filtering process.env
    We should not be removing GIT_ values from the environment if the user
    has set them.  All we now do is set two defaults if they were not
    already set by the end user.

--- test/opts.js
+++ test/opts.js
@@ -1,8 +1,26 @@
 const t = require('tap')
 const gitOpts = require('../lib/opts.js')
-const gitEnv = require('../lib/env.js')
+const gitEnv = {
+  GIT_ASKPASS: 'echo',
+  GIT_SSH_COMMAND: 'ssh -oStrictHostKeyChecking=accept-new'
+}
 
-t.match(gitOpts().env, gitEnv(), 'got the git env by default')
+t.match(gitOpts().env, gitEnv, 'got the git defaults we want')
+
+t.test('does not override', t => {
+  const { GIT_ASKPASS, GIT_SSH_COMMAND } = process.env
+  t.teardown(() => {
+    process.env.GIT_ASKPASS = GIT_ASKPASS
+    process.env.GIT_SSH_COMMAND = GIT_SSH_COMMAND
+  })
+  process.env.GIT_ASKPASS = 'test_askpass'
+  process.env.GIT_SSH_COMMAND = 'test_ssh_command'
+  t.match(gitOpts().env, {
+    GIT_ASKPASS: 'test_askpass',
+    GIT_SSH_COMMAND: 'test_ssh_command',
+  }, 'values already in process.env remain')
+  t.end()
+})
 
 t.test('as non-root', t => {
   process.getuid = () => 999
